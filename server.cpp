34b93b7b8f53c3dee274273604d04508e3a26709
dvenkat <dhruv.venkat.2024@gmail.com>
2026-07-24 15:08:15 -0600

    setup for command-matching

--- server.cpp
+++ server.cpp
@@ -10,6 +10,7 @@
 #include <unordered_map>
 #include <vector>
 #include <sstream>
+#include <cctype>
 
 #define QUEUE_LENGTH 10
 #define MAX_DATA_SIZE 100
@@ -31,12 +32,12 @@ void tokenizeBySpaces(const std::string &input, std::vector<std::string> &tokens
 	while (getline(iss, buf, ' ')) {
 		tokens.push_back(buf);
 	}
-	
+
 	return;
 }
 
 //std::string setCmd(string key, string value, std::unordered_map<string, string> &items) {
-	
+
 //}
 
 //std::string getCmd(string key, std::unordered_map<string, string> &items) {
@@ -60,10 +61,11 @@ int main() {
 
 	char buf[MAX_DATA_SIZE];
 	char incomingStr[MAX_DATA_SIZE];
-	int yes = 1;	
+	int yes = 1;
 
 	std::string pending;
-	
+	char errCmd[] = "ERROR: send messages in the form {COMMAND KEY VALUE}\ne.g. SET name dhruv\n";
+
 	std::vector<std::string> tokens;
 	std::unordered_map<string, string> pairs;
 
@@ -78,7 +80,7 @@ int main() {
 		return 1;
 	}
 
-	
+
 	for (ptr = servinfo; ptr != NULL; ptr = ptr->ai_next) {
 		// Creating the socket connection using the derived addrinfo struct and ensuring that it is valid
 		sockfd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
@@ -103,7 +105,7 @@ int main() {
 	}
 
 	freeaddrinfo(servinfo); // Socket has been configured so we no longer need metadata about the client
-	
+
 	if (ptr == NULL) {
 		std::cerr << "server: no compatible addresses to bind to" << std::endl;
 		return 1;
@@ -122,7 +124,7 @@ int main() {
 		perror("accept");
 		return 2;
 	}
-	
+
 	// Converting the incoming IP address from pure binary to a human-readable format
 	inet_ntop(incomingAddr.ss_family, (struct sockaddr *)&incomingAddr, s, sizeof s);
 	std::cout << "server: receieved connection from " << s << std::endl;
@@ -148,33 +150,10 @@ int main() {
 		std::size_t newlinePos;
 		bool shouldBreak = false;
 
+		// Commands are newline-delimited to avoid issues with TCP latency breaking up commands
 		while ((newlinePos = pending.find('\n')) != std::string::npos) {
 			std::string command = pending.substr(0, newlinePos);
 			pending.erase(0, newlinePos+1);
-
-			tokenizeBySpaces(command, tokens);		
-			std::cout << "server: receieved command " << command << '\n';
-
-			if (tokens.size() < 3) {
-				std::cout << "numtokens: " << tokens.size() << std::endl;
-				if ((send(incomingfd, "OK\n", 3, 0)) == -1) {
-					perror("send");
-					continue;
-				}
-
-				tokens.clear();
-				break;
-			}
-
-			cmd = tokens[0];
-			key = tokens[1];
-			
-			for (int i = 2; i < tokens.size(); i++) {
-				value.append(tokens[i]);
-				value.append(" ");
-			}
-
-			std::cout << "CMD: " << cmd << "  KEY: " << key << "  VAL: " << value << std::endl;
 
 			if (command == "exit") {
 				if (send(incomingfd, "closing connection...", 25, 0) == -1) {
@@ -186,11 +165,57 @@ int main() {
 				break;
 			}
 
+			tokenizeBySpaces(command, tokens);
+			std::cout << "server: receieved command " << command << '\n';
+
+			if (tokens.size() < 2) {
+				if ((send(incomingfd, errCmd, strlen(errCmd), 0)) == -1) {
+					perror("send");
+					continue;
+				}
+
+				tokens.clear();
+				break;
+			}
+
+			cmd = tokens[0];
+			key = tokens[1];
+
+			if (tokens.size() == 2) {
+			    std::cout << "CMD: " << cmd << "  KEY: " << key << std::endl;
+
+				// TODO: Figure out how to normalize cmd to lowercase
+				if (cmd == "GET") {
+				    // search for key-value pair in entries
+				} else if (cmd == "DELETE") {
+				    // delete key-value pair from entries
+				} else {
+				    // TODO: send this out instead of just printing it on server side
+				    std::cout << "ERROR: NOT A COMMAND" << std::endl;
+				}
+
+			} else {
+                for (int i = 2; i < tokens.size(); i++) {
+                    value.append(tokens[i]);
+                    value.append(" ");
+                }
+
+                std::cout << "CMD: " << cmd << "  KEY: " << key << "  VAL: " << value << std::endl;
+
+                if (cmd == "SET") {
+                    // check if the key already exists in the entries and just return that if it is
+                    // else generate a new entry
+                } else {
+                    // TODO: send this back to the client
+                    std::cout << "ERROR: NOT A COMMAND" << std::endl;
+                }
+			}
+
 			if ((send(incomingfd, "OK\n", 3, 0)) == -1) {
 				perror("send");
 				continue;
 			}
-			
+
 			cmd.clear();
 			key.clear();
 			value.clear();
@@ -203,7 +228,7 @@ int main() {
 	}
 
 	close(sockfd);
-	
+
 	return 0;
 
 }
