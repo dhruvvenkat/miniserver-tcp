fefb88f5ba0c8ffa605544f8bbf5e6340eb0f038
dvenkat <dhruv.venkat.2024@gmail.com>
2026-07-23 18:53:34 -0600

    client setup

---
+++ client.cpp
@@ -0,0 +1,20 @@
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define QUEUE_LENGTH 10
+#define MAX_DATA_SIZE 100
+#define PORT "1234"
+
+using namespace std;
+
+int main() {
+
+
+}
